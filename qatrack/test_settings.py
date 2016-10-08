49d7bee1052edc3834fffe0e08e5363c2808696d
Randle Taylor <randle.taylor@gmail.com>
2016-10-08 00:29:49 -0400

    switch to using pytest instead of nose

--- qatrack/test_settings.py
+++ qatrack/test_settings.py
@@ -4,33 +4,6 @@ import os
 from selenium import webdriver
 from .settings import PROJECT_ROOT, INSTALLED_APPS
 
-INSTALLED_APPS += (
-    'django_nose',
-)
-
-COVERAGE_MODULE_EXCLUDES = [
-    'tests$', 'settings$', 'locale$',
-    'common.views.test', '__init__', 'django', 'migrations'
-]
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-TEST_DISCOVER_TOP_LEVEL = PROJECT_ROOT
-TEST_DISCOVER_ROOT = PROJECT_ROOT
-TEST_DISCOVER_PATTERN = "test_*.py"
-NOSE_ARGS = [
-    '--with-coverage',  # activate coverage report
-    # '--with-doctest',  # activate doctest: find and run docstests
-    '--verbosity=1',  # verbose output
-    # '--with-xunit',  # enable XUnit plugin
-    # '--xunit-file=xunittest.xml',  # the XUnit report file
-    '--cover-xml',  # produle XML coverage info
-    '--cover-xml-file=coverage.xml',  # the coverage info file
-    '--cover-package=qatrack.qa,qatrack.units,qatrack.contacts,qatrack.accounts',
-    '--nocapture',
-    '--nologcapture',
-    '--with-id',
-]
-
 NOTIFICATIONS_ON = False
 DEBUG = False
 
